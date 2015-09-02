7220c7400d7c441bf7d9cfab0394b0004cce0393
NoelBao <noeleon930@gmail.com>
2015-09-03 04:16:29 +0800

    lll

--- database/connect_db.js
+++ database/connect_db.js
@@ -1,6 +1,10 @@
 var mongodb = require('mongoskin');
 var client = mongodb.MongoClient;
 
-var db = client.connect('mongodb://localhost:27017/questionnaire');
+var db = client.connect('mongodb://localhost:27017/questionnaire', {
+	server: {
+		poolSize: 12
+	}
+});
 
 module.exports = db;
