93ffd2d4d4d5d3a449eca753b8937e492c27db6f
NoelBao <noeleon930@gmail.com>
2015-09-22 13:58:04 +0800

    ffffffffffffffffff

--- routes/mail.js
+++ routes/mail.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
+var wellknown = require('nodemailer-wellknown');
+var smtpTransport = require('nodemailer-smtp-transport');
 var router = express.Router();
 
 //
@@ -18,6 +20,16 @@ var mail_transporter = nodemailer.createTransport({
 
 });
 
+// var mail_transporter = nodemailer.createTransport(smtpTransport({
+// 	host: "debugmail.io",
+// 	secureConnection: false,
+// 	port: 25,
+// 	auth: {
+// 		user: '101703012@nccu.edu.tw',
+// 		pass: 'a85af2c0-60e8-11e5-8777-9164a8a726e6'
+// 	}
+// }));
+
 //
 //	GET
 //	/:id/
@@ -30,15 +42,17 @@ router.get('/:id', function(req, res, next) {
 		.findById(_id, function(err, rows) {
 
 			var mailOptions = {
-				from: '101703012@mail2.nccu.tw',
+				from: 'info@kpmg.tw',
 				to: rows.email,
-				subject: '我們需要 您的意見與力量，敬請在YYYY/MM/DD前協助完成問卷填寫',
-				text: '親愛的' + rows.name + '同仁，\n\n面臨數位轉型的重要時期，需要 您的意見與力量，請在YYYY/MM/DD前協助完成問卷填寫，\n\n填寫方式：點選您的個人連結  ' + rows._id + ' ，按線上指示逐一填答即可。\n\n本問卷內容僅供統計，不涉個人工作，敬請放心填答，再次感謝 您的回饋！\n\n如有任何填答疑難或問題，請以本郵件回覆並說明情況，我們將盡速回覆您後續處理方式。'
+				subject: '我們需要 您的意見與力量，敬請在 2015/10/08 前協助完成問卷填寫',
+				text: '親愛的' + rows.name + '同仁，\n\n面臨數位轉型的重要時期，需要 您的意見與力量，請在 2015/10/08 前協助完成問卷填寫，\n\n填寫方式：點選您的個人連結  http://140.119.24.13:41224/static/user?uid=' + rows._id + ' ，按線上指示逐一填答即可。\n\n本問卷內容僅供統計，不涉個人工作，敬請放心填答，再次感謝 您的回饋！\n\n如有任何填答疑難或問題，請以本郵件回覆並說明情況，我們將盡速回覆您後續處理方式。'
 			};
 
 			mail_transporter.sendMail(mailOptions, function(err, info) {
 				if (err) {
-					res.json(false);
+					res.json({
+						'error': err
+					});
 				} else {
 					res.json(true);
 				}
